91342b4944ab1da7d95f6a0aa6dc22669ee975a7
ohassanaly <olivierhass@gmail.com>
2026-02-11 16:07:44 +0100

    adding the RAG functionnality

--- src/app.py
+++ src/app.py
@@ -1,6 +1,25 @@
 import streamlit as st
-from utils import retrieve_channel_items
+from utils import retrieve_channel_items, generate_vector_store, fast_rag
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
 import os
+
+load_dotenv()
+
+@st.cache_data(persist="disk", show_spinner=False)
+def get_cached_channel_data(handle):
+    return retrieve_channel_items(handle)
+
+@st.cache_resource
+def get_vectorstore(videos):
+    return generate_vector_store(videos)
+
+@st.cache_resource(show_spinner=False)
+def load_llm():
+    return ChatOpenAI(model="gpt-4o-mini", temperature=0)
+
+if "channel_loaded" not in st.session_state:
+    st.session_state.channel_loaded = False
 
 st.set_page_config(page_title="YouTube Video Summarizer", page_icon="data/yt_logo.png")
 
@@ -18,12 +37,37 @@ run = st.button("Get YT channel videos", type="primary", use_container_width=Tru
 if run:
     try : 
         with st.spinner(f"Retrieving {channel_handle} channel data"):
-            stats, videos = retrieve_channel_items(channel_handle)
-
-            st.text(f"Total #views on the {channel_handle} channel : {stats["viewCount"]}")
-            st.text(f"Total #videos on the {channel_handle} channel : {len(videos)}")
-            st.table(videos)
+            stats, videos = get_cached_channel_data(channel_handle)
+            _ = get_vectorstore(videos)
+            st.session_state.channel_loaded = True
+            st.session_state.channel_handle = channel_handle
 
     except Exception as e:
         st.info("Please check the channel handle you provided")
         print(e)
+
+if st.session_state.channel_loaded:
+    stats, videos = get_cached_channel_data(st.session_state.channel_handle)
+    st.divider()
+    col_a, col_b = st.columns(2)
+    col_a.metric("Total #Views", f"{int(stats['viewCount']):,}")
+    col_b.metric("Total #Videos", len(videos))
+    vectorstore = get_vectorstore(videos) #cached in RAM with st.cache_resource
+    llm = load_llm()
+
+    #RAG Interface
+    user_text = st.text_input(
+    "Ask any question to get video recommendations",
+    placeholder="e.g., What are the latest AI agentic advances?",
+    )
+    run2 = st.button("Recommend videos", type="primary", use_container_width=True)
+    if run2:
+        if user_text:
+            with st.spinner("Searching videos..."):
+                answer = fast_rag(vectorstore, llm, user_text)
+                st.markdown(answer)
+        else: 
+            st.warning("Please enter a question first.")
+
+
+
--- src/utils.py
+++ src/utils.py
@@ -1,10 +1,10 @@
 from googleapiclient.discovery import build
-from dotenv import load_dotenv
+from langchain_community.vectorstores import FAISS
+from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 import os
 
 def retrieve_channel_items(channel_handle:str):
     
-    load_dotenv()
     youtube = build("youtube", "v3", developerKey=os.getenv("YOUTUBE_API_KEY"))
     
     #Step 1: Get the channel’s Uploads playlist ID
@@ -40,3 +40,41 @@ def retrieve_channel_items(channel_handle:str):
         request = youtube.playlistItems().list_next(request, response)
     
     return(statistics, videos)
+
+def generate_vector_store(data):
+
+    title_list = [item["title"] for item in data]
+    url_list = [item["url"] for item in data]
+    metadatas = [{"url": url} for url in url_list]
+
+    embeddings = OpenAIEmbeddings()
+
+    vectorstore = FAISS.from_texts(title_list, embeddings, metadatas=metadatas)
+
+    return(vectorstore)
+
+def fast_rag(vectorstore, llm, user_query, n_videos=10):
+    docs = vectorstore.similarity_search(user_query, k=n_videos)
+    context_blocks = []
+    for i, d in enumerate(docs, 1):
+        url = d.metadata.get("url", "")
+        content = (d.page_content or "").strip()
+        context_blocks.append(f"[video {i}] {content}: {url}".strip())
+
+    context = "\n\n---\n\n".join(context_blocks)
+
+    print(context) #logging in terminal
+
+    prompt = f"""Answer the following question using the provided videos from the YouTube channel
+    Instructions:
+    - Base your answer ONLY on the provided YouTube channel videos.
+    - If some videos are not relevant with user question, don't talk about its. 
+    - Provide URL for each relevant video
+
+    Question: {user_query}
+
+    YouTube channel videos:
+    {context}
+    """
+    answer = llm.invoke(prompt).content
+    return(answer)
